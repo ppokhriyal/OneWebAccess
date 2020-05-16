bb308dca69e7459208991a3748db443db8a90b63
Prashant Pokhriyal <ppokhriyal4@gmail.com>
2020-05-16 07:05:56 +0530

    OneWebAccess

--- onewebaccess/home/routes.py
+++ onewebaccess/home/routes.py
@@ -1,9 +1,15 @@
 from flask import Blueprint,render_template,url_for, flash, redirect, request, abort, session,jsonify
 from onewebaccess import app
+from onewebaccess.home.forms import LoginForm,RegistrationForm
 
 blue = Blueprint('home',__name__,template_folder='templates')
 
 @blue.route('/')
 def home():
+	form = LoginForm()
+	return render_template('home/index.html',title='VXL : OneWebAccess',form=form)
 
-	return render_template('home/index.html',title='VXL : OneWebAccess')
+@blue.route('/signup')
+def signup():
+	form = RegistrationForm()
+	return render_template('home/register.html',title='Sign up',form=form)
