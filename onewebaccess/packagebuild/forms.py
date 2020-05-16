3e14c4a434f71c2638c28446b7c69c70cc644ac4
Prashant Pokhriyal <ppokhriyal4@gmail.com>
2020-05-16 23:26:24 +0530

    OneWebAccess

---
+++ onewebaccess/packagebuild/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,BooleanField,TextAreaField,SelectField,IntegerField
+from wtforms.validators import DataRequired,ValidationError
+from onewebaccess.home.models import User
+
+
+
+class PackageBuildForm(FlaskForm):
+	buildid = StringField('Package Build ID',render_kw={'readonly':True},validators=[DataRequired()])
+	pkgname = StringField('Package Name',validators=[DataRequired()])
+	description = TextAreaField('Description',validators=[DataRequired()])
+	osarch = SelectField('OS Architecture',choices=[('32-Bit','32-Bit'),('64-Bit','64-Bit'),('Multi-Arch','Multi-Arch')])
+	pkgsourcepath = StringField('Package Source',validators=[DataRequired()])
+	patchboolean = BooleanField('Required Patch')
+	patchtype = SelectField('Patch Format',choices=[('Current Patch','Current Patch'),('Legacy Patch','Legacy Patch')])
+	patchname = StringField('Patch Name')
+	removepkg = TextAreaField('Remove Packages')
+	install_script = TextAreaField('Install Script')
+	submit = SubmitField('Build')
--- onewebaccess/packagebuild/routes.py
+++ onewebaccess/packagebuild/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template,url_for, flash, redirect, request, abort, session,jsonify
 from onewebaccess import app,db, bcrypt
-from onewebaccess.home.forms import LoginForm,RegistrationForm
+from onewebaccess.packagebuild.forms import PackageBuildForm
 from flask_login import login_user, current_user, logout_user, login_required
 from onewebaccess.home.models import User
 
@@ -10,7 +10,8 @@ blue = Blueprint('packagebuild',__name__,template_folder='templates')
 def home():
 	return render_template('packagebuild/home.html',title='VXL : Package Builder')
 
-@blue.route('/build')
+@blue.route('/new')
 @login_required
 def build():
-	return render_template('packagebuild/build.html',title='VXL : Package Builder')
+	form = PackageBuildForm()
+	return render_template('packagebuild/build.html',title='VXL : Package Builder',form=form)
